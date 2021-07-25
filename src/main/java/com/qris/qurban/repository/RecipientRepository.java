032226ff5c0177027a0cdaefd86cb77617397d5a
lingga.putra <lingga.putra@homecredit.co.id>
2021-07-25 21:55:43 +0700

    deploy version 0.0.10

--- src/main/java/com/qris/qurban/repository/RecipientRepository.java
+++ src/main/java/com/qris/qurban/repository/RecipientRepository.java
@@ -1,12 +1,15 @@
 package com.qris.qurban.repository;
 
 import com.qris.qurban.model.entity.Recipient;
+import org.springframework.data.domain.Page;
+import org.springframework.data.domain.Pageable;
 import org.springframework.data.jpa.repository.JpaRepository;
 import org.springframework.stereotype.Repository;
 
 @Repository
 public interface RecipientRepository extends JpaRepository<Recipient, Long>
 {
+    Page<Recipient> findByRecipientYearIs(String year, Pageable pageable);
     Recipient findFirstByRecipientEmailIsAndRecipientPhoneIsAndRecipientYearIs(String recipientEmail, String recipientPhone, String recipientYear);
     Recipient findFirstByRecipientEmailIsAndRecipientPasswordIs(String recipientEmail, String recipientPassword);
 }
